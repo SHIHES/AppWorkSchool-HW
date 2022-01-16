22bed7613069734bdbd1517c1e23878101d63f3d
SHIHS <noName@.com>
2022-01-16 18:33:46 +0800

    week 2 HW

---
+++ Week_2/andfun-kotlin-android-trivia/app/src/main/java/com/example/android/navigation/ProfileFragment.kt
@@ -0,0 +1,17 @@
+package com.example.android.navigation
+
+import android.os.Bundle
+import android.view.*
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.android.navigation.databinding.FragmentTitleBinding
+
+class ProfileFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+}
