4caac9ee703053ce515ffefb8dc07757d8095d78
SHIHS <noName@.com>
2022-01-23 17:43:25 +0800

    week 3

---
+++ Week_3/andfun-kotlin-sleep-tracker/app/src/main/java/com/example/android/trackmysleepquality/sleepquality/SleepQualityViewModel.kt
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.trackmysleepquality.sleepquality
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.android.trackmysleepquality.database.SleepDatabaseDao
+import kotlinx.coroutines.*
+
+class SleepQualityViewModel(
+        private val sleepNightKey: Long = 0L,
+        val database: SleepDatabaseDao) : ViewModel() {
+
+    private val _navigateToSleepTracker = MutableLiveData<Boolean?>()
+
+    val navigateToSleepTracker: LiveData<Boolean?>
+        get() = _navigateToSleepTracker
+
+    fun doneNavigating() {
+        _navigateToSleepTracker.value = null
+    }
+
+    val edittextcontent = MutableLiveData<String>()
+
+    private val _textUpdate = MutableLiveData<Boolean?>()
+    val textUpdate : MutableLiveData<Boolean?>
+    get() = _textUpdate
+
+    fun textUpdated (){
+        _textUpdate.value = null
+    }
+
+
+    fun onSetSleepQuality(quality: Int) {
+        viewModelScope.launch {
+                val tonight = database.get(sleepNightKey) ?: return@launch
+                tonight.sleepQuality = quality
+            tonight.userFeedback = edittextcontent.value.toString()
+            database.update(tonight)
+
+
+            // Setting this state variable to true will alert the observer and trigger navigation.
+            _navigateToSleepTracker.value = true
+        }
+    }
+
+}
+
